5796d75512e67408e0eabd991c6b19b374ddac0b
adrien-duchossoy <adrien.duchossoy@icloud.com>
2026-05-16 14:55:48 +0200

    fix: Take static arrows position out of the arrow creation class

--- js/main.js
+++ js/main.js
@@ -1,49 +1,33 @@
 
 class Arrow {
-    constructor (columnIndex) {
+    constructor (columnIndex, arrowsPositionLeft) {
         this.width = 30
         this.height = 30
         this.positionX = 10
         this.positionY = 0 - this.height //had to hard code the -50 otherwise RFA started before DOM
         this.columnIndex = columnIndex
+        this.arrowsPositionLeft = arrowsPositionLeft
         this.arrowElm = null
-        this.arrowsPositionLeft = []
 
-        this.getStaticArrowsPosition()
         this.createArrow()
         this.updateUI()
-    }
-    getStaticArrowsPosition () {
-        const arrows = document.querySelectorAll('.static-arrow')
-        arrows.forEach((arrow) => {
-            let position = arrow.getBoundingClientRect()
-            this.arrowsPositionLeft.push({
-                arrow: arrow.getAttribute('id'),
-                positionLeft: position.left,
-                positionTop: position.top,
-                positionBottom: position.bottom,
-            })
-        })
     }
     createArrow () {
         this.arrowElm = document.createElement('div')
         this.arrowElm.className = 'arrow-down'
         const board = document.getElementById('board')
-        //Potential refactor : take numbers as argument and get this if statement in one line
-        if(this.columnIndex === 'left') {
-            this.arrowElm.style.left = `${this.arrowsPositionLeft[0].positionLeft}px`
-        } else if (this.columnIndex === 'top') {
-            this.arrowElm.style.left = `${this.arrowsPositionLeft[1].positionLeft}px`
-        } else if (this.columnIndex === 'bottom') {
-            this.arrowElm.style.left = `${this.arrowsPositionLeft[2].positionLeft}px`
-        } else if (this.columnIndex === 'right') {
-            this.arrowElm.style.left = `${this.arrowsPositionLeft[3].positionLeft}px`
+        const columnMapping = {
+            left: 0,
+            top: 1,
+            bottom: 2,
+            right: 3,
         }
+        const index = columnMapping[this.columnIndex]
+        this.arrowElm.style.left = `${this.arrowsPositionLeft[index].positionLeft}px`
         board.appendChild(this.arrowElm)  
     }
 
     updateUI () {
-        console.log("arrow created")
         this.arrowElm.style.top = `${this.positionY}px`
         this.arrowElm.style.width = `${this.width}px`
         this.arrowElm.style.heigth = `${this.height}px`
@@ -60,20 +44,35 @@ class ArrowStatic {
 }
 
 const fallingArrows = []
-
 const arrowDirection = ['left', 'top', 'bottom', 'right']
+const getStaticArrowsPosition = () => {
+        const arrows = document.querySelectorAll('.static-arrow')
+        const arrowsPositionLeft = []
+        arrows.forEach((arrow) => {
+            let position = arrow.getBoundingClientRect()
+            arrowsPositionLeft.push({
+                arrow: arrow.getAttribute('id'),
+                positionLeft: position.left,
+                positionTop: position.top,
+                positionBottom: position.bottom,
+            })
+        })
+        return arrowsPositionLeft
+    }
+
+const staticPosition = getStaticArrowsPosition()
 
 arrowDirection.forEach((direction) => {
     setTimeout(() => {
             function spawnArrow () {
                 if (fallingArrows.length < 2) {
-                    const newArrow = new Arrow(direction)
+                    const newArrow = new Arrow(direction, staticPosition)
                     fallingArrows.push(newArrow)
                 }
                     setTimeout(spawnArrow, Math.random() * 10000 + 5000)
             }
             spawnArrow()
-    }, Math.random() * 10000 + 3000)
+    }, Math.random() * 4000 + 1000)
 })
 
 const fall = () => {
@@ -87,6 +86,10 @@ const fall = () => {
         }
     })
         requestAnimationFrame( () => fall())
+}
+
+const detectCollision = () => {
+
 }
 
 fall()
