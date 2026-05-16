5ca575ddfae7cd17483763cef5b892f103f88283
adrien-duchossoy <adrien.duchossoy@icloud.com>
2026-05-16 09:48:09 +0200

    feat: add movement down to arrows

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 
 class Arrow {
-    constructor () {
-        this.width = 20
-        this.height = 20
+    constructor (columnIndex) {
+        this.width = 30
+        this.height = 30
         this.positionX = 10
-        this.positionY = 10
+        this.positionY = -50 - this.height
+        this.columnIndex = columnIndex
         this.arrowElm = null
         this.arrowsPositionLeft = []
 
         this.getStaticArrowsPosition()
         this.createArrow()
         this.updateUI()
-
+        this.fall()
     }
     getStaticArrowsPosition () {
         const arrows = document.querySelectorAll('.static-arrow')
@@ -27,12 +28,29 @@ class Arrow {
         this.arrowElm = document.createElement('div')
         this.arrowElm.className = 'arrow-down'
         const board = document.getElementById('board')
-        this.arrowElm.style.left = `${this.arrowsPositionLeft[0].positionLeft}px`
-        board.appendChild(this.arrowElm)
-        
+        //Potential refactor : take numbers as argument and get this if statement in one line
+        if(this.columnIndex === 'left') {
+            this.arrowElm.style.left = `${this.arrowsPositionLeft[0].positionLeft}px`
+        } else if (this.columnIndex === 'top') {
+            this.arrowElm.style.left = `${this.arrowsPositionLeft[1].positionLeft}px`
+        } else if (this.columnIndex === 'bottom') {
+            this.arrowElm.style.left = `${this.arrowsPositionLeft[2].positionLeft}px`
+        } else if (this.columnIndex === 'right') {
+            this.arrowElm.style.left = `${this.arrowsPositionLeft[3].positionLeft}px`
+        }
+        board.appendChild(this.arrowElm)  
     }
+
     updateUI () {
         console.log("arrow created")
+        this.arrowElm.style.top = `${this.positionY}px`
+        this.arrowElm.style.width = `${this.width}px`
+        this.arrowElm.style.heigth = `${this.height}px`
+    }
+        fall () {
+        this.positionY++
+        this.updateUI()
+        requestAnimationFrame( () => this.fall())
     }
 
 }
@@ -50,4 +68,5 @@ class ArrowStatic {
     new Arrow()
 }, 3000)*/
 
-new Arrow()
+new Arrow('left')
+new Arrow('right')
